6ca34bb2ac8c812ce432a4bf2590c3dc16479341
Faran42 <faranorphys@gmail.com>
2020-03-31 16:00:26 -0300

    featuer: send emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '88375b47bbbc41',
+    pass: '6061eea6502d18',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
